6229b2cdf8caf893d75d80261e26f574ae84d9e4
Riya Suntwal <carrisun91rio@gmail.com>
2020-10-08 14:27:04 +0530

    added randomizertester

---
+++ RandmizerTester.java
@@ -0,0 +1,15 @@
+public class  RandomizerTester{
+public static void main(String[] args){
+	system.out.println("Results os Randomizer.nextInt()");
+	for(int i=0;i<10;i++){
+		System.out.println(Randomizer.nextInt());
+		return int randomizer;
+	}
+	int min=5;
+	int max=10;
+	System.out.println("\nResults of Randomizer.nextInt(5,10)");
+	for(int i=0;i<10;i++){
+		System.out.println(Randomizer.nextInt(min,max));
+	}
+}
+}
